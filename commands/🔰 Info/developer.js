5be1b94ff2da0203668a360da34481ece2b11104
Lorenzo <66470004+lori28167@users.noreply.github.com>
2022-07-26 13:48:12 +0200

    Added Bot files

---
+++ commands/🔰 Info/developer.js
@@ -0,0 +1,48 @@
+const {
+	MessageEmbed
+} = require("discord.js")
+const config = require(`${process.cwd()}/botconfig/config.json`)
+var ee = require(`${process.cwd()}/botconfig/embed.json`)
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+const { MessageButton, MessageActionRow } = require('discord.js')
+const { handlemsg } = require(`${process.cwd()}/handlers/functions`)
+module.exports = {
+	name: "developer",
+	category: "🔰 Info",
+	aliases: ["dev"],
+	description: "Shows Information about the Developer",
+	usage: "developer",	
+	type: "bot",
+	run: async (client, message, args, cmduser, text, prefix) => {
+		let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
+		
+		try {	
+			let button_support_dc = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.developer.buttons.dc).setURL("https://discord.gg/vQ9nf78YAx")
+			const allbuttons = [new MessageActionRow().addComponents([button_public_invite, button_support_dc, button_invite])]
+			message.reply({embeds: [new MessageEmbed()
+				.setColor(es.color)
+				.setFooter(client.getFooter(es))
+				.setTimestamp()
+					.setDescription(client.la[ls].cmds.info.developer.description)],
+components: allbuttons
+			}).catch(error => console.log(error));
+		} catch (e) {
+			console.log(String(e.stack).grey.bgRed)
+			return message.reply({embeds: [new MessageEmbed()
+			  .setColor(es.wrongcolor)
+			  .setFooter(client.getFooter(es))
+			  .setTitle(client.la[ls].common.erroroccur)
+			  .setDescription(eval(client.la[ls]["cmds"]["info"]["color"]["variable2"]))
+			]});
+		}
+	}
+}
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
